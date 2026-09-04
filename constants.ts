38bf69dd24cec4d1d2c03f97407eeea16c3a633f
cf-hcss <cfidan@hampdencharter.org>
2026-09-04 14:04:39 -0400

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -33,7 +33,7 @@ export const ALERTS_SHEET_EDIT_URL: string = 'https://docs.google.com/spreadshee
 // STEP 3: Paste your "Publish to web" CSV link here.
 // This is the link the app fetches data from.
 export const ALERTS_SHEET_CSV_URL =
-  'https://docs.google.com/spreadsheets/d/1AULjkx_6jqNHyuHuEopXJ16b1ofImmjca6PODXO4T3o/gviz/tq?tqx=out:csv';
+  'https://docs.google.com/spreadsheets/d/e/2PACX-1vTh8nY_7LfpcsClqB0jLLiIYML98crfvBxjy6GG2ZHXQ8sx3yVGLEziPRz3rDLk_pVyWSRUy8GsZV7T/pub?gid=0&single=true&output=csv';
 // ===================================================================================
 
 
